0c29fdbdc2bee5920e72727398d4e03a574b45bf
B82hd <64443417+B82hd@users.noreply.github.com>
2020-04-28 05:40:34 +0700

    Update req.php
    Y

--- req.php
+++ req.php
@@ -1,6 +1,6 @@
 <?php
 
-class tri{
+class indosat{
   
   function curls($host,$header,$body,$method)
   	{
@@ -38,7 +38,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
  "Referer:http://bonstri.tri.co.id/login?returnUrl=%2Fhome" ,
  "Accept-Encoding:gzip, deflate" ,
  "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-    $response = $this->curls('http://bonstri.tri.co.id/api/v1/login/request-otp',$header,$body,'POST');
+    $response = $this->curls('http://indosatooredoo.com/api/v1/login/request-otp',$header,$body,'POST');
     return $response;
   }
   
@@ -46,18 +46,18 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
   {
     $body = "grant_type".'='."password".'&'."username".'=' . $msisdn . '&'."password".'='.$otp;
     $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
+    $header = array("Host:indosatooredoo.com" ,
  "Connection:keep-alive" ,
  "Content-Length:" . $ctl ,
  "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
+ "Origin:http://indosatooredoo.com" ,
  "Authorization:Basic Ym9uc3RyaTpib25zdHJpc2VjcmV0" ,
  "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
  "Content-Type:application/x-www-form-urlencoded" ,
- "Referer:http://bonstri.tri.co.id/login?returnUrl=%2Fhome" ,
+ "Referer:http://indosatooredoo.com/login?returnUrl=%2Fhome" ,
  "Accept-Encoding:gzip, deflate" ,
  "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-    $response = $this->curls('http://bonstri.tri.co.id/api/v1/login/validate-otp',$header,$body,'POST');
+    $response = $this->curls('http://indosatooredoo.com/api/v1/login/validate-otp',$header,$body,'POST');
     return $response[1];
   }
   
@@ -68,14 +68,14 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
  "Connection:keep-alive" ,
  "Content-Length:2" ,
  "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
+ "Origin:http://indosatooredoo.com" ,
  "Authorization:Bearer " . $bearer,
  "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
  "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/voucherku" ,
+ "Referer:http://indosatooredoo.com/voucherku" ,
  "Accept-Encoding:gzip, deflate" ,
  "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-   $response = $this->curls("http://bonstri.tri.co.id/api/v1/voucherku/voucher-history",$header,$body,"POST");
+   $response = $this->curls("http://indosatooredoo.com/api/v1/voucherku/voucher-history",$header,$body,"POST");
    return $response[1];
    
   }
@@ -85,18 +85,18 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
     $body = array("rewardId"=>$id1,"rewardTransactionId"=>$id);
     $body = json_encode($body);
     $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
+    $header = array("Host:indosatooredoo.com" ,
  "Connection:keep-alive" ,
  "Content-Length:" . $ctl ,
  "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
+ "Origin:http://indosatooredoo.com" ,
  "Authorization:Bearer " . $bearer ,
  "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
  "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/voucherku" ,
+ "Referer:http://indosatooredoo.com/voucherku" ,
  "Accept-Encoding:gzip, deflate" ,
  "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-     $response = $this->curls("http://bonstri.tri.co.id/api/v1/voucherku/get-voucher-code",$header,$body,"POST");
+     $response = $this->curls("http://indosatooredoo.com/api/v1/voucherku/get-voucher-code",$header,$body,"POST");
      return $response[1];
   }
   
